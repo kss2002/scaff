e25601d1b17e2e438886fc5ccb2f789e7448b4ed
kss2002 <sc00310@naver.com>
2025-09-01 11:47:01 +0900

    ci test code modify

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,10 @@
+# test/controllers/home_controller_test.rb
 require "test_helper"
 
+# CI modify
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get root_url  # home_index_url 대신 root_url 사용
     assert_response :success
   end
 end
